d52a31d25a1762551ae1b18d2d134a9d0f8ce75d
SaleemTheKing <saleem031998@gmail.com>
2021-04-13 11:18:02 +0200

    Change directory outputs

--- conf/ci/buildapp.sh
+++ conf/ci/buildapp.sh
@@ -4,5 +4,5 @@ cd ./git-src
 chmod 755 ./sbt-dist/bin/sbt
 
 ./sbt-dist/bin/sbt dist
-mkdir ../zip
-mv target/universal/rws-web-dd-oper-1.0-SNAPSHOT.zip ../zip
+mkdir ../play-app
+mv target/universal/rws-web-dd-oper-1.0-SNAPSHOT.zip ../play-app
